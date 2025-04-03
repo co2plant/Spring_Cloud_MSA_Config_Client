5015317fccd9b1a27be89b199fbf28c4b4b3b55f
co2plant <93631841+co2plant@users.noreply.github.com>
2025-04-03 09:16:48 +0900

    init: init project

---
+++ src/main/java/kr/co/inntavern/spring_cloud_msa_config_client/SpringCloudMsaConfigClientApplication.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.spring_cloud_msa_config_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsaConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMsaConfigClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/inntavern/spring_cloud_msa_config_client/SpringCloudMsaConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.spring_cloud_msa_config_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMsaConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
