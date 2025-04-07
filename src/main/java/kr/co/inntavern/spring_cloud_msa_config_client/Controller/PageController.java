91dd041dd63e61996ac836e4f90e0b1750ea9599
co2plant <93631841+co2plant@users.noreply.github.com>
2025-04-07 15:02:14 +0900

    fix: Chapter 10

--- src/main/java/kr/co/inntavern/spring_cloud_msa_config_client/Controller/PageController.java
+++ src/main/java/kr/co/inntavern/spring_cloud_msa_config_client/Controller/PageController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/msa")
 public class PageController {
-    @GetMapping("/first")
+    @GetMapping("/msa/first")
     public String testPage(){
-        return "Hello World! Spring Cloud MSA Config Client!";
+        return "MSA first";
     }
 }
